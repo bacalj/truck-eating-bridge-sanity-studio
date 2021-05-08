dcb545ce1d6e3d1970a25473337aea7fd62ec36a
Joseph Bacal <bacalj@gmail.com>
2021-05-08 16:29:29 -0400

    schema for presentations

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,16 +1,48 @@
-// First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
 
-// Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
+
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
+
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      name: "presentation",
+      type: "document",
+      title: "Presentations",
+      fields: [
+        {
+          name: "title", 
+          type: "string", 
+          title: "Title",
+          validation: Rule => Rule.required()
+        },
+        {
+          name: "slug",
+          title: "Slug",
+          type: "slug",
+          options: {
+            source: 'title',
+            maxLength: 96
+          }
+        },
+        {
+          title: 'Poster',
+          name: 'poster',
+          type: 'image',
+        },
+        {
+          title: 'Published Google Slides URL',
+          name: 'slidesUrl',
+          type: 'url'
+        },
+        {
+          title: 'Published Google Sites URL',
+          name: 'sitesURL',
+          type: 'url'
+        }
+      ]
+    }
   ])
 })
